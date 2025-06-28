141f06eaf6d3cefb17ced3e764f3eb9f7ea66634
Piyush Kumar <piyushbagoria018@gmail.com>
2025-06-28 10:54:26 +0530

    Add ToastContainer for notifications in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import "./App.css";
+import './App.css';
 import Navbar from "./componets/Navbar";
 import Hero from "./componets/Hero/Hero";
 import Skills from "./componets/Skills/Skills";
 import MyProjects from "./componets/MyProjects/MyProjects";
 import ContactMe from "./componets/ContactMe/ContactMe";
 import Footer from "./componets/Footer/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
       <ContactMe/>
       </div>
       <Footer/>
+      <ToastContainer position="top-right" autoClose={2500} theme="colored" />
     </>
   );
 }
